b30afeafc2e0905c46a2ff227d75414f3d45e51c
roman <Roman.Savelyev@norbit.ru>
2022-05-11 22:01:48 +0300

    5.2 1:07:37

--- manager/src/Model/User/Service/ResetTokenSender.php
+++ manager/src/Model/User/Service/ResetTokenSender.php
@@ -6,8 +6,32 @@ namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
 use App\Model\User\Entity\User\ResetToken;
+use Twig\Environment;
 
-interface ResetTokenSender
+class ResetTokenSender
 {
-    public function send(Email $email, ResetToken $resetToken): void;
+    private \Swift_Mailer $mailer;
+    private Environment $twig;
+    private array $from;
+
+    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->from = $from;
+    }
+
+    public function send(Email $email, ResetToken $resetToken): void
+    {
+        $message = (new \Swift_Message('Password resetting'))
+            ->setFrom($this->from)
+            ->setTo($email->getValue())
+            ->setBody($this->twig->render('mail/user/reset.html.twig', [
+                'token' => $resetToken->getToken()
+            ]), 'text/html');
+
+        if (!$this->mailer->send($message)) {
+            throw new \RuntimeException('Unable to send message.');
+        }
+    }
 }
